ff32d2c16aa979c53dbe996121aea1b6ae3a9d9d
prosayj@gmail.com <15665662468@163.com>
2020-04-16 13:49:46 +0800

    Refactor:源码编译配置修改 Feat:新增测试模块：thinking-in-spring

---
+++ thinking-in-spring/src/main/java/com/prosayj/basecode/thinkinginspring/overview/dependency/lookup/DependecyLookupDemo.java
@@ -0,0 +1,26 @@
+package com.prosayj.basecode.thinkinginspring.overview.dependency.lookup;
+
+import com.prosayj.basecode.thinkinginspring.overview.domain.User;
+
+/**
+ * 依赖查找测试
+ *
+ * @author yangjian@bubi.cn
+ * @date 2020-04-16 上午 02:03
+ * @since 1.0.0
+ */
+public class DependecyLookupDemo {
+	public static void main(String[] args) {
+		//配置xml文件
+		//启动应用上下文
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext("MATE-INF/dependecy-lookup-context.xml");
+
+
+		//实时查找
+		User user = beanFactory.getBean("user", User.class);
+		System.out.println("user = " + user);
+
+
+
+	}
+}
---
+++ thinking-in-spring/src/main/java/com/prosayj/basecode/thinkinginspring/overview/domain/User.java
@@ -0,0 +1,37 @@
+package com.prosayj.basecode.thinkinginspring.overview.domain;
+
+/**
+ * TODO
+ *
+ * @author yangjian@bubi.cn
+ * @date 2020-04-16 上午 02:08
+ * @since 1.0.0
+ */
+public class User {
+	private int id;
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
