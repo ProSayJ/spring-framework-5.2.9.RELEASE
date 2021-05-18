7eb4850b0b091e8c0994dd06f120a800c2e72c9f
prosayj@gmail.com <15665662468@163.com>
2021-05-18 11:09:47 +0800

    refactor:demo测试调整
    refactor:demo测试调整

--- 00-spring_source_analysis/src/test/java/IocTest.java
+++ 00-spring_source_analysis/src/test/java/IocTest.java
@@ -11,16 +11,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class IocTest {
 	public static void main(String[] args) {
-		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-		MyBean lagouBean = applicationContext.getBean(MyBean.class);
-		System.out.println(lagouBean);
+		testIoC();
+//		testAOP();
 	}
 
 	/**
 	 * Ioc 容器源码分析基础案例
 	 */
-	@Test
-	public void testIoC() {
+	public static void testIoC() {
 		// ApplicationContext是容器的高级接口，BeanFacotry（顶级容器/根容器，规范了/定义了容器的基础行为）
 		// Spring应用上下文，官方称之为 IoC容器（错误的认识：容器就是map而已；准确来说，map是ioc容器的一个成员，
 		// 叫做单例池, singletonObjects,容器是一组组件和过程的集合，包括BeanFactory、单例池、BeanPostProcessor等以及之间的协作流程）
@@ -32,16 +30,15 @@ public class IocTest {
 		 * Bean后置处理器初始化：AbstractApplicationContext#refresh#registerBeanPostProcessors
 		 */
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
-		MyBean lagouBean = applicationContext.getBean(MyBean.class);
-		System.out.println(lagouBean);
+		MyBean myBean = applicationContext.getBean(MyBean.class);
+		System.out.println(myBean);
 	}
 
 
 	/**
-	 * Ioc 容器源码分析基础案例
+	 * AOP 容器源码分析基础案例
 	 */
-	@Test
-	public void testAOP() {
+	public static void testAOP() {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 		MyBean myBean = applicationContext.getBean(MyBean.class);
 		myBean.print();
